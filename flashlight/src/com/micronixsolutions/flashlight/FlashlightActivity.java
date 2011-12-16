098ad58b281d750024476f85f99a182952f37eaf
Juan Carlos Ferrer <contact@micronixsolutions.com>
2011-12-15 19:20:20 -0600

    Initial commit

---
+++ flashlight/src/com/micronixsolutions/flashlight/FlashlightActivity.java
@@ -0,0 +1,13 @@
+package com.micronixsolutions.flashlight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FlashlightActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
