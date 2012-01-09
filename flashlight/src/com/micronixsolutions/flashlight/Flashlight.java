8a778c152d5b98425a0a763268dfddfa222f1154
Juan Carlos Ferrer <contact@micronixsolutions.com>
2012-01-09 01:05:19 -0600

    Added new 'Flashlight' class that handles all the dirty details. Flashlight on/off is much faster now, does stuff in a thread. Flashlight is working on Xoom, but on GNex, it still needs a stupid surfaceview.  Fix it later.

---
+++ flashlight/src/com/micronixsolutions/flashlight/Flashlight.java
@@ -0,0 +1,132 @@
+package com.micronixsolutions.flashlight;
+
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+import java.util.List;
+
+public class Flashlight implements SurfaceHolder.Callback {
+    private static final String TAG = "Flashlight";
+    private int camId = -1;
+    private boolean openCameraFail = false;
+    private Camera cam = null;
+    private Camera.Parameters parameters = null;
+    private boolean on = false; //State of the flashlight
+    
+    //Open the camera in a thread, and make sure that it supports 'torch' mode
+    Thread openCameraThread = new Thread(new Runnable(){
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            try{
+                Log.d(TAG, "Opening cameraId: " + camId);
+                cam = Camera.open(camId);
+            } catch (RuntimeException e){
+                Log.d(TAG, e.toString());
+                openCameraFail = true;
+                return; //TODO: Do something more appropriate
+            } 
+            parameters = cam.getParameters();
+            List<String> flash_modes = parameters.getSupportedFlashModes();
+            Log.d(TAG, "Supported flash modes: " + flash_modes);
+            if(flash_modes == null | !flash_modes.contains("torch")){
+                openCameraFail = true;
+                cam = null;
+                return; //TODO: Do something more appropriate..this class is useless to you
+            }
+            
+            // Flashlight user could have called 'setTorch(true)' while
+            // we were initializing the camera...check for the state, and turn it on
+            if(on)
+                setTorch(true); //turn the flashlight on
+        }
+    });
+    
+    /* CONSTRUCTOR
+     * 
+     */
+    public Flashlight(){
+        for(int i=0; i < Camera.getNumberOfCameras(); i++){
+            //Assume that only rear facing cameras have a flash...pick the first one we find.
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            Log.d(TAG, "Camera info: " + info);
+            if(info.facing == Camera.CameraInfo.CAMERA_FACING_BACK){
+                camId = i;
+                break;
+            }
+        }
+        if(camId == -1){
+            //TODO: No camera found, raise exception or something... this class is useless to you
+            openCameraFail = true;
+            return;
+        }
+        
+        init();
+    }
+    
+    /** Call this to manually open the camera...if you previously called 'stop'
+     * 
+     */
+    public void init(){
+        //Begin opening the camera
+        if(cam == null)
+            openCameraThread.start();
+    }
+    
+    /** Call this stop the flashlight, and release the camera
+     * 
+     */
+    public void stop(){
+        //release the camera
+        if(cam != null){
+            cam.release();
+            cam = null;
+        }
+    }
+    /* MAGIC HAPPENS HERE
+     * 
+     */
+    public void setTorch(Boolean on){
+        Log.d(TAG, "Flashlight setTorch: " + on);
+        this.on = on;
+        if(on && cam != null){
+            Log.d(TAG, "Starting Preview");
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            cam.setParameters(parameters);
+            cam.startPreview();
+        }
+        else if (cam !=null) {
+            Log.d(TAG, "Stopping Preview");
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            cam.setParameters(parameters);
+            cam.stopPreview();
+        }
+    }
+
+    /* SURFACE HOLDER CALLBACK INTERFACE
+     * 
+     */
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        Log.d(TAG, "surfaceCreated");
+        
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // TODO Auto-generated method stub
+        Log.d(TAG, "surfaceChanged");
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        Log.d(TAG, "surfaceDestroyed");
+
+        
+    }
+}
--- flashlight/src/com/micronixsolutions/flashlight/FlashlightActivity.java
+++ flashlight/src/com/micronixsolutions/flashlight/FlashlightActivity.java
@@ -3,24 +3,17 @@ package com.micronixsolutions.flashlight;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.hardware.Camera;
 
-import java.io.IOException;
+import com.micronixsolutions.flashlight.Flashlight;
+
 
 public class FlashlightActivity extends Activity implements OnCheckedChangeListener {
     public static final String TAG = "FlashlightApp";
-    
-    private Camera cam = null;
-    private SurfaceView preview = null;
-    private SurfaceHolder previewHolder = null;
-    private Boolean surface_exists = false;
     private ToggleButton toggleButton = null;
+    private Flashlight flashlight = null;
     
     /** Called when the activity is first created. */
     @Override
@@ -28,72 +21,29 @@ public class FlashlightActivity extends Activity implements OnCheckedChangeListe
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.main);
-
+        
+        //Set the toggle button's on checked listener to this...
         toggleButton = (ToggleButton) findViewById(R.id.toggleButton1);
         toggleButton.setOnCheckedChangeListener(this);
-       
-        preview = (SurfaceView) findViewById(R.id.surfaceView1);
-        previewHolder = (SurfaceHolder) preview.getHolder();
-        previewHolder.addCallback(surfaceCallback);
-        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         
+        flashlight = new Flashlight(); //Sweet new flashlight object
         
+        //The toggle button could be 'on' if we rotated device, and activity was destroyed/created
+        if(toggleButton.isChecked())
+            flashlight.setTorch(true);
     }
     
-    SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
-        
-        @Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-            // TODO Auto-generated method stub
-            Log.d(TAG, "surfaceDestroyed");
-            surface_exists = false;
-            if(cam != null ){
-                cam.release();
-                Log.d(TAG, "camera released");
-
-            }
-        }
-        
-        @Override
-        public void surfaceCreated(SurfaceHolder holder) {
-            // TODO Auto-generated method stub
-            Log.d(TAG, "surfaceCreated");
-            surface_exists = true;
-        }
-        
-        @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            // TODO Auto-generated method stub
-            Log.d(TAG, "surfaceChanged");
-            setTorch(toggleButton.isChecked());
-        }
-    };
-
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        //The flashlight holds and instance of a camera...make sure to stop it
+        flashlight.stop();
+    }
+    
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         // TODO Auto-generated method stub
         Log.d(TAG, "onCheckedChanged: " + isChecked);
-        setTorch(isChecked);
-    }
-    
-    public void setTorch(Boolean on){
-        Log.d(TAG, "Setting torch mode to: " + on);
-        if(on && surface_exists){
-            cam = Camera.open();
-            Camera.Parameters parameters = cam.getParameters();
-            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            cam.setParameters(parameters);
-            try {
-                cam.setPreviewDisplay(previewHolder);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            cam.startPreview();
-        }
-        else{
-            if(cam != null)
-                cam.release();
-        }
+        flashlight.setTorch(isChecked);
     }
 }
